ed092c0b605d268b5cf812a426f138c09b4f5c0f
Burhan Rabbani <burhanit11@gmail.com>
2024-07-25 20:56:24 -0700

    cors add

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import usersRouter from "./routes/users.routes.js";
+import cors from "cors";
 
 const app = express();
 
+var corsOptions = {
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: "http://localhost:3000/",
+  Credential: true,
+  optionsSuccessStatus: 200,
+};
+
 // middleware
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
