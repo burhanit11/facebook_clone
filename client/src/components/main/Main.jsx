0387a8d609514c5d04f81dff9846c10a53901526
Burhan Rabbani <burhanit11@gmail.com>
2024-07-27 09:05:00 -0700

    .

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -9,14 +9,11 @@ import profilePic from "../../../public/images/profile-picture.webp";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-
+import Divider from "@mui/material/Divider";
+import VideocamIcon from "@mui/icons-material/Videocam";
 import Image from "next/image";
-import { Search } from "@mui/icons-material";
+import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
+import MoodIcon from "@mui/icons-material/Mood";
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
@@ -50,17 +47,6 @@ const DemoPaper = styled(Paper)(({ theme }) => ({
   border: "1px solid #e1e3e5",
   borderRadius: "5px",
 }));
-const DemoPaper2 = styled(Paper)(({ theme }) => ({
-  width: 550,
-  height: 80,
-  padding: theme.spacing(2),
-  display: "flex",
-  alignItems: "center",
-  // justifyContent: "center",
-  border: "1px solid #e1e3e5",
-  borderRadius: "8px",
-  margin: "auto",
-}));
 
 export default function Main() {
   return (
@@ -81,7 +67,42 @@ export default function Main() {
         </DemoPaper>
       </Stack>
 
-      <div square>
+      <div className="shadow-lg border-2 rounded-lg w-[95%] mx-auto px-2">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+            p: 2,
+          }}
+        >
+          <Image
+            src={profilePic}
+            height={50}
+            width={50}
+            className="rounded-full"
+          />
+          <div className="bg-grayColor mx-2 p-4 rounded-full w-full text-xl">
+            Whats on your mind, Burhan?
+          </div>
+        </Box>
+        <Divider />
+        <Box sx={{ display: "flex", p: 2, gap: 2 }}>
+          <div className="flex items-center">
+            <VideocamIcon fontSize={"large"} color="error" />{" "}
+            <span className="text-xl font-medium">Live video</span>{" "}
+          </div>
+          <div className="flex items-center">
+            <PhotoLibraryIcon fontSize={"large"} color="success" />{" "}
+            <span className="text-xl font-medium">Photo/video</span>{" "}
+          </div>
+          <div className="flex items-center">
+            <MoodIcon fontSize={"large"} color="warning" />
+            <span className="text-xl font-medium">Feeling/Activity</span>{" "}
+          </div>
+        </Box>
+      </div>
+      <div className="shadow-lg border-2 rounded-lg w-[95%] mx-auto px-2 mt-5">
         <Box
           sx={{
             display: "flex",
@@ -91,24 +112,24 @@ export default function Main() {
         >
           <Image
             src={profilePic}
-            height={50}
-            width={50}
+            height={60}
+            width={60}
             className="rounded-full"
           />
-          <Search
-            sx={{ backgroundColor: "#f0f2f5", color: "black", m: 0 }}
-            className="rounded-"
-          >
-            <SearchIconWrapper>
-              <SearchIcon className="text-black" />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <div className="mx-2 rounded-full w-full p-2 ">
+            <h1 className="text-xl font-medium">
+              Burhan . <span className="text-primary">Follow</span>{" "}
+            </h1>
+            <p className="text-lg">
+              Recommended post ·{" "}
+              <span className="text-grayColor2">3 days ago</span>
+            </p>
+          </div>
         </Box>
-        <Box></Box>
+        <Divider />
+        <Box sx={{ display: "flex" }}>
+          <Image src={profilePic} />
+        </Box>
       </div>
     </div>
   );
